5bcf75aca8a96eae07f6ba6b633166137e2c3daa
Andrew Casarsa <acasarsa@gmail.com>
2021-10-18 19:08:25 -0400

    reverse string solved without using .reverse

---
+++ module.js
@@ -0,0 +1,7 @@
+export function string() {
+  return "Hello"
+}
+
+export function array() {
+  return [1, 200, 40, 19, 23]
+}
---
+++ reverseString.js
@@ -0,0 +1,15 @@
+import {string} from './module.js';
+
+const str = string()
+
+function reverse(str) {
+  let reversed = ''
+  
+  for (var i = str.length - 1; i >= 0; i--){
+    reversed += str[i]
+  }
+  console.log(reversed.trim())
+  return reversed.trim()
+}
+reverse(str)
+// would be better to see if the beginning was empty space but how to avoid getting rid of intended white space?
